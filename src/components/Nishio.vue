d6164f0176acb12b903ad0b28ea361ad84065a29
kazzool <kazzool1583@gmail.com>
2019-04-30 21:31:26 +0900

    realtimeDBとの紐付けテスト

---
+++ src/components/Nishio.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id = 'nishio'>
+    <h1>240</h1>
+    <img src="../assets/nishio.jpg">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nishio'
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
